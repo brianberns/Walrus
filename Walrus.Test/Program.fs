4af24e3942bcff3b280a6200bc904d58960ae85d
Brian Berns <brianberns@gmail.com>
2023-12-05 22:22:25 -0500

    Simplified.

--- Walrus.Test/Program.fs
+++ Walrus.Test/Program.fs
@@ -5,5 +5,5 @@ open Walrus
 module Program =
 
     Csv.loadTable @"C:\Users\brian\Downloads\train.csv"
-        |> Table.pivot<int, int> "Pclass" "Survived" "PassengerId" Seq.length
+        |> Table.pivot "Pclass" "Survived" "PassengerId" Seq.length
         |> Table.print
