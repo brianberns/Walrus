5260b7f9ba5513b7c5bf93a01bec0a98054a2cfd
Brian Berns <brianberns@gmail.com>
2023-12-12 01:20:30 -0500

    Commented.

--- Walrus.Test/Program.fs
+++ Walrus.Test/Program.fs
@@ -1,25 +1,4 @@
 module Program
 
-open Walrus
+People.``Country counts`` ()
 
-[<EntryPoint>]
-let main argv =
-
-    let byClass =
-        Table.loadCsv "titanic.csv"                // load Titanic data from a CSV file
-            |> Table.pivot ["Pclass"] "Survived"   // count the # of survivors in each passenger class
-            |> Table.sortRowsBy ["Pclass"]         // sort the resulting pivot table's rows
-            |> Table.renameColumns                 // give each column a meaningful name
-                [ "Pclass"; "Died"; "Survived" ]
-    let byClass =                                  // add a column for the # of passengers in each class
-        byClass?Died + byClass?Survived
-            |> Table.ofColumn "Total"
-            |> Table.unionColumns byClass
-    Table.ofColumns                                // compute percentages instead of raw counts
-        [
-            "Passenger class", byClass?Pclass
-            "Died (%)", round (byClass?Died / byClass?Total * 100.)
-            "Survived (%)", round (byClass?Survived / byClass?Total * 100.)
-        ] |> Table.print                           // print the result
-
-    0
