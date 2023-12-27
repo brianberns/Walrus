3db9a97016dbb409599938448ccfe85cd5fea7d6
Brian Berns <brianberns@gmail.com>
2023-12-26 23:37:02 -0500

    Added test.

--- Walrus.Test/Csv.fs
+++ Walrus.Test/Csv.fs
@@ -5,12 +5,20 @@ open Xunit
 open Walrus
 
 /// https://theeventscalendar.com/knowledgebase/csv-file-examples-for-importing/
-[<Fact>]
-let Events () =
+let events = Table.loadCsvFile "CSV-file-for-example.csv"
 
-    let events = Table.loadCsvFile "CSV-file-for-example.csv"
+[<Fact>]
+let ``Start date`` () =
     let column =
         Table.getColumn<DateOnly> "EVENT START DATE" events
     let expected = DateOnly.Parse("6/25/2022")
+    let actual = column.Values[0]
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Start time`` () =
+    let column =
+        Table.tryGetColumn<TimeOnly> "EVENT START TIME" events
+    let expected = TimeOnly.Parse("3:30pm") |> Some
     let actual = column.Values[0]
     Assert.Equal(expected, actual)
